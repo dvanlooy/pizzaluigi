7cd1c1b5d5ca9ae22f89aa5de62325cd40814c11
dvanlooy <dvanlooy@DESKTOP-CGIRFOQ.lan>
2016-08-01 12:55:13 +0200

    encderedirecturl

--- src/be/vdab/servlets/PizzaToevoegenServlet.java
+++ src/be/vdab/servlets/PizzaToevoegenServlet.java
@@ -60,7 +60,8 @@ public class PizzaToevoegenServlet extends HttpServlet {
 				String pizzaFotosPad = this.getServletContext().getRealPath("/pizzafotos");
 				fotoPart.write(String.format("%s/%d.jpg", pizzaFotosPad, pizza.getId()));
 			}
-			response.sendRedirect(String.format(REDIRECT_URL, request.getContextPath()));
+			response.sendRedirect(response.encodeRedirectURL(
+					String.format(REDIRECT_URL, request.getContextPath())));
 		} else {
 			request.setAttribute("fouten", fouten);
 			request.getRequestDispatcher(VIEW).forward(request, response);
